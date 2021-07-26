1be716f5ba292694b2b0c8cde4e6fa533fe475ce
ldmoore <ldmoore0205@gmail.com>
2021-07-26 18:24:46 -0400

    Upload profile pictures

---
+++ functions/aws.js
@@ -0,0 +1,34 @@
+const { LightsailClient, AllocateStaticIpCommand } = require("@aws-sdk/client-lightsail");
+const s3 = require("s3");
+const crypto = require("crypto");
+const fs = require("fs");
+
+const client = s3.createClient({
+	maxAsyncS3: 20, // this is the default 
+	s3RetryCount: 3, // this is the default 
+	s3RetryDelay: 1000, // this is the default 
+	multipartUploadThreshold: 20971520, // this is the default (20 MB) 
+	multipartUploadSize: 15728640, // this is the default (15 MB) 
+	s3Options: {
+		// Using the keys from our AWS IAM user
+		accessKeyId: process.env.AWS_ID,
+		secretAccessKey: process.env.AWS_SECRET,
+		
+		// any other options are passed to new AWS.S3() 
+		// See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property 
+	},
+});
+
+module.exports = {
+	uploadImage: async function(uid, file) {
+			const params = {
+			  localFile: file.path,
+			  s3Params: {
+				Bucket: process.env.AWS_BUCKET,
+				Key: `users/${uid}/${file.name}`, // File path of location on S3
+				ACL: "public-read"
+			  },
+			};
+			return client.uploadFile(params);
+	}
+}
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,11 @@
 const express = require("express"), router = express.Router();
 const User = require("../../schemas/user.model");
 const Site = require("../../schemas/site.model");
-const { checkProfileInfo } = require("../../functions/checks");
+const { checkProfileInfo, checkAdmin } = require("../../functions/checks");
+const { uploadImage } = require("../../functions/aws");
+const formidable = require("formidable");
+const crypto = require("crypto");
+const fs = require("fs");
 
 router.post("/", async (req, res) => {
 	//Create new user
@@ -34,13 +38,14 @@ router.post("/", async (req, res) => {
 		created: user.global.created,
 		attained_badges: user.global.attained_badges,
 		bio: user.global.bio,
+		avatar: user.global.avatar,
 		email: auth ? user.global.email : "",
 		_id: user._id
 	});
 }).post("/:user/edit", async (req, res) => {
 	const user = await User.findOne({ "global.username": req.params.user });
 	if (!user) return res.sendStatus(404);
-	console.log(req.body);
+	if (req.user.global.persist_token !== user.global.persist_token && !checkAdmin(req.user)) return res.sendStatus(403);
 	let hi = [req.body.name, req.body.username, req.body.email];
 	if (!hi.every(i => i)) return res.redirectWithFlash(`/profile/${req.user.global.username}`, { error: "You must complete the name, username, and email fields" });
 	let error = await checkProfileInfo(req.body.name, req.body.username, req.body.bio, req.body.email, null, user);
@@ -51,6 +56,31 @@ router.post("/", async (req, res) => {
 	user.global.email = req.body.email;
 	await user.save();
 	return res.redirectWithFlash(`/profile/${user.global.username}`, { message: "Profile edited!" });
+}).post("/:user/avatar", async (req, res) => {
+	const user = await User.findOne({ "global.username": req.params.user });
+	if (!user) return res.sendStatus(404);
+	if (req.user.global.persist_token !== user.global.persist_token && !checkAdmin(req.user)) return res.sendStatus(403);
+	formidable.IncomingForm().parse(req)
+		.on("fileBegin", (name, file) => {
+			let ext = file.name.slice((file.name.lastIndexOf(".") - 1 >>> 0) + 2);
+			file.name = `${crypto.randomBytes(32).toString("hex")}.${ext}`;
+			file.path = `tmp/${file.name}`;
+		}).on("file", async (name, file) => {
+			if (!["png", "gif", "jpeg", "jpg", "webp"].includes(file.name.slice((file.name.lastIndexOf(".") - 1 >>> 0) + 2))) return res.redirectWithFlash(`/profile/${user.global.username}`, { error: "Your avatar must be an image!" });
+			let uploader = await uploadImage(req.user._id.toString(), file);
+			uploader.on("error", function(err) {
+				console.log("S3 upload error", err.stack);
+				return res.redirectWithFlash(`/profile/${req.user.global.username}`, { error: "There was an error uploading your avatar" });
+			});
+			uploader.on("end", async function() {
+				fs.unlink(file.path, function (err) {
+					if (err) console.log(err);
+				});
+				user.global.avatar = `https://${process.env.AWS_BUCKET}.s3.us-east-1.amazonaws.com/users/${req.user._id}/${file.name}`;
+				await user.save();
+				return res.redirectWithFlash(`/profile/${user.global.username}`, { message: "Avatar edited!" });
+			});
+		});
 }).get("/:user/hackathons", async (req, res) => {
 	const user = await User.findOne({ "global.username": req.params.user });
 	if (!user) return res.sendStatus(404);
